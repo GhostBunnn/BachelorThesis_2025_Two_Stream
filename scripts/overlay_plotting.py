ae74862febab10c183a20393254a980094b11ae4
Marco Zullich <marco.zullich@gmail.com>
2025-08-03 00:54:37 +0200

    overlay plotting all pruning ratios

--- scripts/overlay_plotting.py
+++ scripts/overlay_plotting.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import pandas as pd
-import numpy as np
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -9,21 +7,33 @@ sys.path.append(BASE_DIR)
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from PIL import Image
-from torchvision import transforms
 
-video_folder = "spatial_v_CliffDiving_g18_c04"
-video_rgb = "v_CliffDiving_g18_c04"
-frames_dir = os.path.join(BASE_DIR, "data", "extracted_rgb_frames", video_rgb)
-print(frames_dir)
-overlay_dir = os.path.join(BASE_DIR, "plots", "overlay_outputs", video_folder)
-irof_path = os.path.join(BASE_DIR, "plots", "irof_results", video_folder, 'irof_scores.csv')
+def parse_prune_ratio(prune_str):
+    if prune_str == "unpruned":
+        return 0.0
+    return float(prune_str.replace("percent", "").replace("_", "."))
+
+# plotting settings
+base_font_size = 20
+plt.rc('font', size = base_font_size)
+plt.rc('axes', linewidth=3, titlesize = base_font_size+2, labelsize = base_font_size+2)
+plt.rc('xtick', top=True, bottom=True, direction='in')
+plt.rc('ytick', left=True, right=True, direction='in') 
+plt.rc('figure', titlesize=base_font_size+4, dpi=300)
+plt.rc('legend', fontsize=base_font_size-1, title_fontsize=base_font_size-1, frameon=False)
+plt.rc('lines', linewidth=3)
+
+video_folder = "spatial_v_Skijet_g16_c04"
+video_rgb = "v_Skijet_g16_c04"
 savefig_path = os.path.join(BASE_DIR, "plots", "irof_plots")
-
-irof_data = pd.read_csv(irof_path, header=None, names=["frame_file", "irof_auc"])
-irof_data["irof_auc"] = irof_data["irof_auc"].astype(str).str.replace(",", " ").astype(float)
-
-irof_array = pd.to_numeric(np.asarray(irof_data["irof_auc"]), errors='coerce')
-nr_rows = 3
+frames_dir = os.path.join(BASE_DIR, "data", "extracted_rgb_frames", video_rgb)
+run_path = os.path.join(BASE_DIR, "plots", "overlay_outputs", "run1")
+pruning_dir_list = [x for x in  os.listdir(run_path) if os.path.isdir(os.path.join(run_path, x))]
+nr_rows = 2 + len(pruning_dir_list)
+pruning_dir_list.sort(key=lambda z: (z != "unpruned", z))
+overlay_dir_list = []
+for entry in pruning_dir_list:
+    overlay_dir_list.append(os.path.join(run_path,entry))
 
 frame_files = sorted([
     f for f in os.listdir(frames_dir)
@@ -34,33 +44,46 @@ nr_frames = sum(1 for f in os.listdir(frames_dir) if f.endswith(".jpg") or f.end
 fig, ax = plt.subplots(nr_rows, nr_frames, figsize=(4*nr_frames, 4*nr_rows), constrained_layout=True)
 
 for i, frame_file in enumerate(frame_files):
+    
     # Load images
     frame_img = Image.open(os.path.join(frames_dir, frame_file))
-    overlay_img = Image.open(os.path.join(overlay_dir, f"overlay_frame{i}.png"))
-
+    
     # Show original frame
-    im = ax[0, i].imshow(frame_img)
-    ax[0, i].set_title(f"IROF = {irof_array[i]}")
-    ax[0, i].xaxis.set_visible(False)
+    ax[0, i].imshow(frame_img)
     
     # Show resized frame
     resized_frame = frame_img.resize((224, 224), resample=Image.BILINEAR)
     ax[1, i].imshow(resized_frame)
+    
+    for j, overlay_dir in enumerate(overlay_dir_list):  
+        overlay_img = Image.open(os.path.join(overlay_dir, "spatial", f"{video_folder}", f"overlay_frame_000{i}.jpg"))
 
-    # Show overlay
-    ax[2, i].imshow(overlay_img)
-    if i!= 0:
-        ax[0, i].yaxis.set_visible(False)
-        ax[1, i].yaxis.set_visible(False)
-        ax[2, i].yaxis.set_visible(False)
+        # Show overlay
+        ax[j+2, i].imshow(overlay_img)
+
+prune_ratios = []
+for y in pruning_dir_list:        
+    prune_ratios.append(parse_prune_ratio(y))
+prune_ratios = sorted(prune_ratios)
+for r in range(nr_rows):
+    for c in range(nr_frames):
+        if c != 0:
+            ax[r, c].yaxis.set_visible(False)
+        if r != nr_rows-1:
+            ax[r, c].xaxis.set_visible(False)
+        if r == 0:
+            ax[r,0].set_ylabel("Original Frame")
+        elif r == 1:
+            ax[r,0].set_ylabel("Resized Frame")
+        else:
+            ax[r,0].set_ylabel(f"Overlay - {prune_ratios[r-2]}%")
         
 norm = plt.Normalize(vmin=0, vmax = 1)
 sm = cm.ScalarMappable(cmap='jet', norm=norm)
 sm.set_array([])
 
 char = fig.colorbar(sm, ax=ax.ravel().tolist(), shrink=0.6, aspect=50, pad=0.02)
-char.set_label('Saliency Intensity')
+# char.set_label('Saliency Intensity')
 
 # plt.tight_layout()
-plt.suptitle(f"Average IROF = {np.mean(irof_array):.6f}")
-plt.savefig(savefig_path + '/' + video_rgb + "_irof_combined.png")
+plt.savefig(savefig_path + '/overlay_saliency_combined_' + video_rgb + ".png")
